ae728d3c4393863117c2467fa660c2d1009677f4
David Lynam <dlynam@gmail.com>
2019-01-16 11:48:01 -0800

    update product parser

--- parsers/products.rb
+++ parsers/products.rb
@@ -10,15 +10,17 @@ product['title'] = nokogiri.at_css('#productTitle').text.strip
 product['seller'] = nokogiri.at_css('a#bylineInfo').text.strip
 
 #extract number of reviews
-reviews_count = nokogiri.at_css('span#acrCustomerReviewText').text.strip.split(' ').first.gsub(',','')
-product['reviews_count'] = reviews_count =~ /^[0-9]*$/ ? reviews_count.to_i : nil
+reviews_text = nokogiri.at_css('span#acrCustomerReviewText')
+reviews_count = reviews_text ? nokogiri.at_css('span#acrCustomerReviewText').text.strip.split(' ').first.gsub(',','') : nil
+product['reviews_count'] = reviews_count =~ /^[0-9]*$/ ? reviews_count.to_i : 0
 
 #extract rating
-stars_num = nokogiri.at_css('#averageCustomerReviews span.a-icon-alt').text.strip.split(' ').first
+rating_text = nokogiri.at_css('#averageCustomerReviews span.a-icon-alt')
+stars_num = rating_text ? nokogiri.at_css('#averageCustomerReviews span.a-icon-alt').text.strip.split(' ').first : nil
 product['rating'] = stars_num =~ /^[0-9.]*$/ ? stars_num.to_f : nil
 
 #extract price
-product['price'] = nokogiri.at_css('#price_inside_buybox').text.strip.gsub('$','').to_f
+product['price'] = nokogiri.at_css('#price_inside_buybox', '#priceblock_ourprice', '#priceblock_dealprice').text.strip.gsub(/[\$,]/,'').to_f
 
 #extract availability
 product['available'] = nokogiri.at_css('#availability').text.strip == 'In Stock.' ? true : false
@@ -26,7 +28,7 @@ product['available'] = nokogiri.at_css('#availability').text.strip == 'In Stock.
 #extract product description
 description = ''
 nokogiri.css('#feature-bullets li').each do |li|
-  unless li['id'] || li['class']
+  unless li['id'] || (li['class'] && li['class'] != 'showHiddenFeatureBullets')
     description += li.text.strip + ' '
   end
 end
